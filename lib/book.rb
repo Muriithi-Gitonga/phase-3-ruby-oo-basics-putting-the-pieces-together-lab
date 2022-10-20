eb7065cc856182bae71c580b821a2e929c73db17
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-10-20 15:47:14 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count, :turn_page
+    def initialize (title)
+        @title = title
+    end
+
+     def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
 
 
